78015d243bb18c4b951059b82852fbd32ed6f205
André Cheng <138728580+chengandre@users.noreply.github.com>
2023-12-13 22:54:03 +0000

    removed globals from AS

--- AS.cpp
+++ AS.cpp
@@ -7,20 +7,17 @@
 
 using namespace std;
 
-int fd_tcp, fd_udp, tcp_child, errcode;
-ssize_t n_tcp, n_udp;
+int fd_tcp, fd_udp;
 socklen_t addrlen;
 struct addrinfo hints, *res;
 struct sockaddr_in addr;
-string hostname, port, ip, input;
-char buffer[BUFFERSIZE];
-vector<string> inputs;
+string port;
 bool verbose = false;
 time_t fulltime;
 struct tm *current_time;
 string current_time_str;
 SharedAID *sharedAID;
-char host[NI_MAXHOST],service[NI_MAXSERV];
+char host[NI_MAXHOST], service[NI_MAXSERV];
 
 
 bool exists(string& name) {
@@ -284,7 +281,7 @@ bool checkAuctionDuration(string const &aid) {
     return true;
 }
 
-string handleUDPRequest(char request[], char host[], char service[]) {
+string handleUDPRequest(char request[]) {
 
     // handle UDP request from user, return the response to send
     string response;
@@ -636,7 +633,7 @@ void startUDP() {
         exit(EXIT_FAILURE);
     }
 
-    n_udp = ::bind(fd_udp, res->ai_addr, res->ai_addrlen);
+    int n_udp = ::bind(fd_udp, res->ai_addr, res->ai_addrlen);
     if (n_udp == -1) {
         cout << "[LOG]: UDP Bind error: " << strerror(errno);
         exit(EXIT_FAILURE);
@@ -658,6 +655,7 @@ void startUDP() {
 
     cout << "[LOG]: UDP starting to read from socket" << endl;
     string response;
+    char buffer[BUFFERSIZE];
     while (true) {
         addrlen = sizeof(addr);
         response.clear();
@@ -668,10 +666,11 @@ void startUDP() {
             cout << "Error reading request from UDP socket" << endl;
             response = "ERR\n";
         } else {
+            int errcode;
             if ((errcode = getnameinfo((struct sockaddr *) &addr, addrlen, host, sizeof(host), service, sizeof(service), NI_NUMERICHOST)) != 0) {
                 cout << "[LOG]: UDP Error getnameinfo: " << gai_strerror(errcode) << endl;
             } 
-            response = handleUDPRequest(buffer, host, service);
+            response = handleUDPRequest(buffer);
         }
 
         cout << "[LOG]: UDP sending response: " << response;
@@ -833,7 +832,7 @@ void deleteAuctionDir(string &aid) {
     removeDir(AID_dirname);
 }
 
-string getNextAID(SharedAID *sharedAID) {
+string getNextAID() {
     // returns the next available AID
 
     sem_wait(&sharedAID->sem); // prevents other processes from getting an aid
@@ -897,7 +896,7 @@ bool checkOwner(string &uid, string &aid) {
     return strcmp(uid.c_str(), tmp);
 }
 
-void handleTCPRequest(int &fd, SharedAID *sharedAID, char host[], char service[]) {
+void handleTCPRequest(int &fd) {
     // handles TCP request from user (receives, handles and answers)
 
     string request, tmp, response;
@@ -938,7 +937,7 @@ void handleTCPRequest(int &fd, SharedAID *sharedAID, char host[], char service[]
                         cout << "[LOG]: Incorrect password" << endl;
                         response += "NOK\n";
                     } else {
-                        string aid = getNextAID(sharedAID);
+                        string aid = getNextAID();
 
                         createAuctionDir(aid);
                         receiveTCPfile(fd, fsize, request_arguments.at(5), aid);
@@ -1193,7 +1192,7 @@ void handleTCPRequest(int &fd, SharedAID *sharedAID, char host[], char service[]
     close(fd);
 }
 
-void startTCP(SharedAID *sharedAID) {
+void startTCP() {
 
     fd_tcp = socket(AF_INET, SOCK_STREAM, 0);
     if (fd_tcp == -1) {
@@ -1208,7 +1207,7 @@ void startTCP(SharedAID *sharedAID) {
         exit(EXIT_FAILURE);
     }
 
-    n_tcp = ::bind(fd_tcp, res->ai_addr, res->ai_addrlen);
+    int n_tcp = ::bind(fd_tcp, res->ai_addr, res->ai_addrlen);
     if (n_tcp == -1) {
         cout << "[LOG]: TCP Bind error" << endl;
         exit(EXIT_FAILURE);
@@ -1221,6 +1220,8 @@ void startTCP(SharedAID *sharedAID) {
     }
 
     int ret;
+    int tcp_child;
+    int errcode;
     while (true) {
         addrlen = sizeof(addr);
         cout << "[LOG]: Parent " << getpid() << " starting to accept requests" << endl;
@@ -1248,11 +1249,11 @@ void startTCP(SharedAID *sharedAID) {
         if (setsockopt(tcp_child, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout, sizeof(timeout)) == -1) {
             cout << "[LOG]: " << getpid() << " Error setting timeout" << endl;
             do {
-                ret = close(tcp_child)  ;
+                ret = close(tcp_child);
             } while (ret == -1 && errno == EINTR);
             exit(EXIT_FAILURE);
         }
-
+        
         pid_t pid = fork();
         if (pid == -1) {
             cout << "TCP fork error" << endl;
@@ -1266,7 +1267,7 @@ void startTCP(SharedAID *sharedAID) {
                 cout << "[LOG]: TCP Error getnameinfo: " << gai_strerror(errcode) << endl;
             }
             
-            handleTCPRequest(tcp_child, sharedAID, host, service);
+            handleTCPRequest(tcp_child);
             if (shmdt(sharedAID) == -1) {
                 cout << "[LOG]: " << getpid() << " erro detaching shared memory" << endl;
             }
@@ -1353,7 +1354,7 @@ int main(int argc, char *argv[]) {
     hints.ai_socktype = SOCK_DGRAM;
     hints.ai_flags = AI_PASSIVE;
 
-    errcode = getaddrinfo(NULL, port.c_str(), &hints, &res);
+    int errcode = getaddrinfo(NULL, port.c_str(), &hints, &res);
     if (errcode != 0) {
         cout << gai_strerror(errcode);
         exit(EXIT_FAILURE);
@@ -1374,7 +1375,7 @@ int main(int argc, char *argv[]) {
         startUDP();
     } else {
         signal(SIGINT, SIG_DFL);
-        startTCP(sharedAID);
+        startTCP();
     }
     
     return 0;
